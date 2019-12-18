732a486e39171c98afd5c848f0bc7b2af76217b5
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2019-12-18 13:18:36 -0500

    javascript practice 2 deliverables done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskForm = document.querySelector("#create-task-form")
+
+  const taskInput = document.querySelector("#new-task-description")
+
+
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const taskList = document.querySelector('#tasks')
+    const taskItem = document.createElement('li')
+    taskItem.innerText = taskInput.value
+
+    const priorityLevel = document.querySelector("#priorityLevel")
+
+    console.log(priorityLevel.value);
+
+    if (priorityLevel.value === "red") {
+      taskItem.className = "red"
+    } else if (priorityLevel.value === "yellow"){
+      taskItem.className = "yellow"
+    } else if (priorityLevel.value === "green"){
+      taskItem.className = "green"
+    }
+
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "Delete"
+    taskItem.appendChild(deleteButton)
+
+    deleteButton.addEventListener("click", (e) => {
+      const parent = e.target.parentNode
+      parent.remove()
+    })
+
+
+    taskList.appendChild(taskItem)
+    taskForm.reset()
+
+  })
+
+})
